4502f17f6f8f7cf88fb56c031905f8e662d28ea5
Gabriel Roy <gab@uon.io>
2018-09-14 01:55:58 -0400

    First commit

---
+++ index.ts
@@ -0,0 +1,34 @@
+
+/*! *******************************************************************************
+Copyright (C) Gabriel Roy <g@uon.io>. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+********************************************************************************* */
+/*! @uon/geom */
+
+export * from './src/ConcaveHull';
+export * from './src/ConvexHull';
+export * from './src/Delaunay';
+export * from './src/Line2';
+export * from './src/Path2';
+export * from './src/Polygon2';
+export * from './src/Segment2';
+export * from './src/Tessellator2';
+export * from './src/Triangle2';
